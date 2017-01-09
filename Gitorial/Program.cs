0cb3939ef257721a8dfb4cd50903665ea1afc2e7
PolM <PolM@PC216-08>
2017-01-09 10:58:05 +0100

    123

--- Gitorial/Program.cs
+++ Gitorial/Program.cs
@@ -15,7 +15,6 @@ namespace Gitorial
             //commentaar
             //nog commentaar
             //Yannick en David hebben zich ziek gemeld, jammer!
-            //Ik heb nog commentaar toegevoegd
         }
     }
 }
