d1bca50f7e144d0a651ae72fdc42a5c6c9455350
JulianM <JulianM@PC216-06>
2017-01-09 10:53:37 +0100

    Test nog is

--- Gitorial/Program.cs
+++ Gitorial/Program.cs
@@ -15,7 +15,7 @@ namespace Gitorial
             //commentaar
             //nog commentaar
             //Yannick en David hebben zich ziek gemeld, jammer!
-            //Nog Commentaar ll
+            //Nog Commentaar lol
         }
     }
 }
